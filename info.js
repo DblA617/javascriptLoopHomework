7513c589440dcbbb02d5908bf0bccfa7cb1c6e6d
Aarika Turner <turneraarika@gmail.com>
2021-01-26 04:19:39 -0500

    push

---
+++ info.js
@@ -0,0 +1,19 @@
+var newArray = [];
+for (var i = 1; i < 11; i++){
+    newArray.push(i);
+}
+var ourArray = [];
+var i = 10
+while (i >= 1) {
+    ourArray.push(i); i--;
+}
+var someArray = [];
+for (var i = 1; i < 10; i += 2) {
+    someArray.push(i);
+}
+var thisArray = [];
+for (var i = 15; i > 0; i -= 2) {
+    thisArray.push(i);
+}
+
+
